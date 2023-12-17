4031bb143c1909c9fc4e0ea44cbbbb7e02a494a5
JixTheCat <bryce.polley@gmail.com>
2023-12-17 21:29:38 +1030

    saving after pmgpy does not have optimisations.

--- bn.py
+++ bn.py
@@ -1,52 +1,60 @@
+import dash
+import visdcc
+import pandas as pd
+from dash import html
+from dash import dcc
+from dash.dependencies import Input, Output, State
+from populate import populate
+
 import networkx as nx
 import matplotlib.pyplot as plt
 from pgmpy.models import BayesianNetwork
 from pgmpy.factors.discrete.CPD import TabularCPD
 
-# Define the Bayesian Network structure
-student = BayesianNetwork([('diff', 'grades'), ('aptitude', 'grades')])
+# # Define the Bayesian Network structure
+# student = BayesianNetwork([('diff', 'grades'), ('aptitude', 'grades')])
 
-# Define the Conditional Probability Distribution for the 'grades' node
-grades_cpd = TabularCPD('grades'
-                        , 3
-                        , [
-                            [0.1, 0.2, 0.3, 0.8, 0.6, 0.4]
-                            , [0.1, 0.2, 0.3, 0.1, 0.2, 0.3]
-                            , [0.8, 0.6, 0.4, 0.1, 0.2, 0.3]]
-                            ,  evidence=['diff', 'aptitude']
-                            , evidence_card=[2, 3]
-                            , state_names={'grades': ['gradeA', 'gradeB', 'gradeC']
-                                , 'diff': ['easy', 'hard']
-                                , 'aptitude': ['low', 'medium', 'high']
-                                })
+# # Define the Conditional Probability Distribution for the 'grades' node
+# grades_cpd = TabularCPD('grades'
+#                         , 3
+#                         , [
+#                             [0.1, 0.2, 0.3, 0.8, 0.6, 0.4]
+#                             , [0.1, 0.2, 0.3, 0.1, 0.2, 0.3]
+#                             , [0.8, 0.6, 0.4, 0.1, 0.2, 0.3]]
+#                             ,  evidence=['diff', 'aptitude']
+#                             , evidence_card=[2, 3]
+#                             , state_names={'grades': ['gradeA', 'gradeB', 'gradeC']
+#                                 , 'diff': ['easy', 'hard']
+#                                 , 'aptitude': ['low', 'medium', 'high']
+#                                 })
 
-diff_cpd = TabularCPD('diff'
-                        , 2
-                        , [
-                            [0.9] # easy
-                            ,[0.1]] # hard
-                            , state_names={'diff': ['easy', 'hard']
-                                })
+# diff_cpd = TabularCPD('diff'
+#                         , 2
+#                         , [
+#                             [0.9] # easy
+#                             ,[0.1]] # hard
+#                             , state_names={'diff': ['easy', 'hard']
+#                                 })
 
-aptitude_cpd = TabularCPD('aptitude'
-                        , 3
-                        , [
-                            [0.2] # low
-                            ,[0.6] # medium
-                            ,[0.2]] # high
-                            , state_names={'aptitude': ['low', 'medium', 'high']
-                                })
-# # Add the CPD to the Bayesian Network
-student.add_cpds(grades_cpd)
-student.add_cpds(diff_cpd)
-student.add_cpds(aptitude_cpd)
+# aptitude_cpd = TabularCPD('aptitude'
+#                         , 3
+#                         , [
+#                             [0.2] # low
+#                             ,[0.6] # medium
+#                             ,[0.2]] # high
+#                             , state_names={'aptitude': ['low', 'medium', 'high']
+#                                 })
+# # # Add the CPD to the Bayesian Network
+# student.add_cpds(grades_cpd)
+# student.add_cpds(diff_cpd)
+# student.add_cpds(aptitude_cpd)
 
 # # Print the Bayesian Network structure
 # print(student)
 
 # # Print the CPDs for each node
-for cpd in student.get_cpds():
-    print(f"\nCPD for Node {cpd.variable}:\n{cpd}")
+# for cpd in student.get_cpds():
+#     print(f"\nCPD for Node {cpd.variable}:\n{cpd}")
 
 # for state in grades_cpd.state_names['grades']:
 #     print(student.get_state_probability({'grades': state}))
@@ -56,18 +64,13 @@ for cpd in student.get_cpds():
 # nx.draw(student, pos, with_labels=True, node_size=2000, node_color="skyblue", font_size=10, font_color="black", font_weight="bold", arrowsize=20)
 # plt.show()
 
-import dash
-import visdcc
-import pandas as pd
-from dash import html
-from dash import dcc
-from dash.dependencies import Input, Output, State
+BN = populate()
+# BN = student
 
 app=dash.Dash()
 
-# df = pd.read_csv("data.csv")
-node_list = student.nodes
-edge_list = student.edges
+node_list = BN.nodes
+edge_list = BN.edges
 
 nodes = [
     {
@@ -109,7 +112,6 @@ app.layout = html.Div(
         )
     ])
 
-
 @app.callback(
     Output('nodes', 'children'),
     [Input('net', 'selection')])
@@ -121,24 +123,27 @@ def myfun(x):
         # for state in student.get_cpds(id).state_names[id]:
         #     s += str(state) + ': '
         #     s += str(student.get_state_probability({id: state}))
-        #     s += '\n'
-        s = [(html.B(str(id)))]
-        # s = [str(id)]
-        # s.append(str(id))
 
+        s = [(html.B(str(id)))]
         s.append(html.Br())
         s.append(html.Br())
-        for state in student.get_cpds(id).state_names[id]:
-            s.append(str(state) + ': ')
-            s.append(str(student.get_state_probability({id: state})))
-            s.append(html.Br())
+
+        # s.append(BN.get_cpds(id).state_names[id])
+        s.append(' '.join(BN.get_cpds(id).state_names[id]))
+        s.append(str(BN.get_state_probability({id: 'ideal'})))
+        # s.append('hello')
+
+        # for state in BN.get_cpds(id).state_names[id]:
+        #     s.append(str(state) + ': ')
+        #     s.append(str(BN.get_state_probability({id: state})))
+        #     s.append(html.Br())
+        s.append(html.Br())
         s.append(html.Br())
     except ValueError:
         return "No node selected"
     except IndexError:
         return "No node selected"
     return s
-
 
 @app.callback(
     Output('net', 'options'),
--- populate.py
+++ populate.py
@@ -1,8 +1,9 @@
-import string
-
-BN = BayesianNetwork()
-
-df = pd.read_excel('CPT.xlsx', sheet_name='CPT', header=None)
+import networkx as nx
+import matplotlib.pyplot as plt
+from pgmpy.models import BayesianNetwork
+from pgmpy.factors.discrete.CPD import TabularCPD
+from cpt import calculate_CPT
+from pandas import read_excel
 
 def zero_string(element):
     if type(element) == str:
@@ -12,58 +13,64 @@ def zero_string(element):
 def typesafe_isnan(element):
     return isinstance(element, float) and element != element
 
-num_var = len(df) // 4
-for i in range(0, num_var):
-    node_name = ' '.join([str(elem) for elem in df.iloc[4*i+1, 0].split()[:-1]])
-    BN.add_node(node_name)
+def populate():
 
-    parents = [parent for parent in df.iloc[4*i, 3:] if not typesafe_isnan(parent)]
-    for parent in parents:
-        BN.add_edge(parent, node_name)
+    BN = BayesianNetwork()
 
-    ideal = df.iloc[4*i+1,1]
-    not_ideal = df.iloc[4*i+2,2]
-    if typesafe_isnan(not_ideal):
-        not_ideal = 1-ideal
+    df = read_excel('CPT.xlsx', sheet_name='CPT', header=None)
 
-    weights = list(df.iloc[4*i+1, 3:])
-    weights = [zero_string(weight) for weight in weights if not typesafe_isnan(weight)]
+    num_var = len(df) // 4
+    for i in range(0, num_var+1):
+        node_name = ' '.join([str(elem) for elem in df.iloc[4*i+1, 0].split()[:-1]]).lower()
+        BN.add_node(node_name)
 
-    if weights == []:
-        ideal_cpt = [ideal]
-        not_ideal_cpt = [not_ideal]
-    elif sum(weights) == 0:
-        ideal_cpt = calculate_CPT([1 for weight in weights], ideal, 1-ideal)
-        not_ideal_cpt = calculate_CPT([1 for weight in weights], not_ideal, 1-not_ideal) 
-    else:
-        ideal_cpt = calculate_CPT(weights, ideal, 1-ideal)
-        ideal_cpt = [0.5 if prob == 0 else prob for prob in ideal_cpt]
-        not_ideal_cpt = calculate_CPT(weights, not_ideal, 1-not_ideal)
-        not_ideal_cpt = [0.5 if prob == 0 else prob for prob in not_ideal_cpt]
+        parents = [parent.lower() for parent in df.iloc[4*i, 3:] if not typesafe_isnan(parent)]
+        for parent in parents:
+            BN.add_edge(parent, node_name)
 
-    state_names = {node_name: ['ideal', 'not_ideal']}
-    for parent in parents:
-        state_names[parent] = ['ideal', 'not_ideal']
+        ideal = df.iloc[4*i+1,1]
+        not_ideal = df.iloc[4*i+1,2]
+        if typesafe_isnan(not_ideal):
+            not_ideal = 1-ideal
 
-    if parents = []:
-        cpds = TabularCPD(node_name
-            , 2
-            , [
-                ideal_cpt
-                , not_ideal_cpt
-            ]
-            , state_names =state_names
-        )
-    else:
-        evidence = list(state_names.keys())[1:]
-        cpds = TabularCPD(node_name
-            , 2
-            , [
-                ideal_cpt
-                , not_ideal_cpt
-            ]
-            , evidence=list(state_names.keys())[1:]
-            , evidence_card=[2 for evidence in evidence]
-            , state_names=state_names
-        )
-    BN.add_cpds(cpds)
+        weights = list(df.iloc[4*i+1, 3:])
+        weights = [zero_string(weight) for weight in weights if not typesafe_isnan(weight)]
+
+        if weights == []:
+            ideal_cpt = [ideal]
+            not_ideal_cpt = [1-ideal]
+        elif sum(weights) == 0:
+            ideal_cpt = calculate_CPT([1 for weight in weights], ideal, 1-ideal)
+            not_ideal_cpt = [1-prob for prob in ideal_cpt]
+        else:
+            ideal_cpt = calculate_CPT(weights, ideal, 1-ideal)
+            ideal_cpt = [0.5 if prob == 0 else prob for prob in ideal_cpt]
+            not_ideal_cpt = [1-prob for prob in ideal_cpt]
+
+        state_names = {node_name: ['ideal', 'not_ideal']}
+        for parent in parents:
+            state_names[parent] = ['ideal', 'not_ideal']
+
+        if parents == []:
+            cpds = TabularCPD(node_name
+                , 2
+                , [
+                    ideal_cpt
+                    , not_ideal_cpt
+                ]
+                , state_names =state_names
+            )
+        else:
+            evidence = list(state_names.keys())[1:]
+            cpds = TabularCPD(node_name
+                , 2
+                , [
+                    ideal_cpt
+                    , not_ideal_cpt
+                ]
+                , evidence=list(state_names.keys())[1:]
+                , evidence_card=[2 for evidence in evidence]
+                , state_names=state_names
+            )
+        BN.add_cpds(cpds)
+    return BN

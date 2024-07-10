38daeff09c33f0d23039fc5bd7ded044f68fdef3
JixTheCat <bryce.polley@gmail.com>
2024-07-10 13:47:07 +0930

    recovery from pm2 running added and fixed path to backup daemon

--- start.sh
+++ start.sh
@@ -14,8 +14,9 @@ sudo cp -f /usr/share/nginx/html/nginx.conf /etc/nginx/nginx.conf
 sudo systemctl stop httpd
 sudo systemctl start nginx
 sudo npm install -g pm2
+sudo pm2 stop server
 sudo pm2 start server.js --cwd /usr/share/nginx/html/src/
 sudo apt-get install inotify-tools
 cd /usr/share/nginx/html
-sudo chmod +x /path/to/daemon-script.sh
-nohup /path/to/daemon-script.sh &
+sudo chmod +x /usr/share/nginx/html/backup.sh
+nohup /usr/share/nginx/html/backup.sh &
